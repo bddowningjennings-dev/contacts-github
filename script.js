e8fb70f6b34c458fe06041e304ddaea3fcd8a426
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-07-26 15:05:33 -0700

    working new user

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("form").submit(function( event ) {
+  $("#user-form").submit(function( event ) {
     var user = $("#username").val();
   event.preventDefault();
         console.log("got the jQ")
@@ -21,5 +21,20 @@ $(document).ready(function() {
           })
       })
         .fail(function() {alert('errrr')});
+  })
+  $("#new-form").submit(function( event ) {
+    event.preventDefault();
+      $("aside").append(
+            "<div class='contact'><div> <img src='putin.png' alt='putin'>" +
+            "<p>Name: " + $("#first-name").val() + " " + $("#last-name").val() +
+            "</p></div><div class='hidden'>Bio:<br>" + $("#bio").val() +
+            "</div></div><br>" 
+          );
+        $('.hidden').hide();
+        $('.contact').off('click');
+        $('.contact').click(function() {
+            $(this).children().toggle();
+            // console.log('clicked');
+          })
   })
 })
