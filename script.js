530978351ae6efedf20c749a844a7f79a337b252
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-07-26 11:52:30 -0700

    for group

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 $(document).ready(function() {
   $("form").submit(function( event ) {
+    var user = $("#username").val();
   event.preventDefault();
-  $("tbody").append(
-    "<tr><td>"+$("#first-name").val()+
-    "<td>"+$("#last-name").val()+
-    "<td>"+$("#email").val()+
-    "<td>"+$("#contact-number").val()+
-    "</td></tr>"
-  );
-  $("form input").val("");
-  $("form #submit").val("Add User");
+        console.log("got the jQ")
+        var url = "https://api.github.com/users/" + user;
+        $.getJSON(url, function(data) {
+          console.log(data);
+          $("aside").append(
+            "<div class='contact'> <img src='" + data.avatar_url +
+            "'><p>Username: " + user +
+            "</p><p>Name: " + data.name +
+            "</p></div>" 
+          )
+        })
   })
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// $('#myForm').submit(function() {
-//     // get all the inputs into an array.
-//     var $inputs = $('#myForm :input');
-
-//     // not sure if you wanted this, but I thought I'd add it.
-//     // get an associative array of just the values.
-//     var values = {};
-//     $inputs.each(function() {
-//         values[this.name] = $(this).val();
-//     });
-
-// });
