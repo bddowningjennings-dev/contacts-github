de7b8be5d25b24eb4132826644f1a1f4a701baef
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-07-26 00:43:55 -0700

    working user project

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  $("form").submit(function( event ) {
+  alert( "Handler for .submit() called." );
+  event.preventDefault();
+  $("tbody").append(
+    "<tr><td>"+$("#first-name").val()+
+    "<td>"+$("#last-name").val()+
+    "<td>"+$("#email").val()+
+    "<td>"+$("#contact").val()+
+    "</td></tr>"
+  );
+  $("form input").val("");
+  $("form #submit").val("Add User");
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#myForm').submit(function() {
+//     // get all the inputs into an array.
+//     var $inputs = $('#myForm :input');
+
+//     // not sure if you wanted this, but I thought I'd add it.
+//     // get an associative array of just the values.
+//     var values = {};
+//     $inputs.each(function() {
+//         values[this.name] = $(this).val();
+//     });
+
+// });
